5a70f55b253688051552da739a4d7387d8ac6551
Sandrima <84657254+Sandrima@users.noreply.github.com>
2021-06-19 21:33:17 +0530

    updated query4.sql

--- query4.sql
+++ query4.sql
@@ -1,4 +1,6 @@
--- ### All books on Technology
--- Create a file called `query4.sql` which will contain a query that will
--- print all the subjects of the book with name "Atomic Habits".
-select s.name from subjects s, books_subjects bs, books b where bs.subject = s.id and bs.book = b.id and b.title = "Atomic Habits";
+select subjects.name
+from subjects
+join books_subjects
+join books
+on books_subjects.subject = subjects.id and books.id = books_subjects.book
+where books.title = 'Atomic Habits';
