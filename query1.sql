b76aa89a56b24d0d4fc06e45fe133f84515b0f64
Sandrima <84657254+Sandrima@users.noreply.github.com>
2021-06-19 21:29:50 +0530

    updated query1.sql

--- query1.sql
+++ query1.sql
@@ -1,4 +1,1 @@
--- ### All books by PHI
--- Creating a file called `query1.sql` which will contain a query that
--- will print the names of all books by published by PHI.
-select b.title from books b, publisher p where b.publisher = p.id and p.name = "PHI";
+select title from books as b inner join publisher as p on p.id = b.publisher where b.publisher = 1;
