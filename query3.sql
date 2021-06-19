5a736628b0c36ddc22ab2bfb660b568d64aa5166
Sandrima <84657254+Sandrima@users.noreply.github.com>
2021-06-19 21:32:20 +0530

    updated query3.sql

--- query3.sql
+++ query3.sql
@@ -1,5 +1,7 @@
--- ### All books on Technology or Politics
--- Creating a file called `query3.sql` which will contain a query that
--- will print the names of all the books on "Technology" or
--- "Politics". (hint consider using the IN condition in your where clause).
-select b.title from books b, subjects s, books_subjects bs where bs.book = b.id and bs.subject = s.id and s.name in ("Technology", "Politics");
+select b.title
+from books as b
+inner join books_subjects as bs
+on b.id = bs.book
+inner join subjects as s
+on bs.subject = s.id
+where s.name IN ('Technology','Politics');
